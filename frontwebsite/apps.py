fed1c26f3b300af7743db001082b2a3b3e0932ec
Anand Shukla <mydevplanet@gmail.com>
2023-08-07 04:30:25 -0700

    First commit

---
+++ frontwebsite/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FrontwebsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'frontwebsite'
---
+++ frontwebsite/static/main.js
@@ -0,0 +1,7 @@
+ new Splide( '.splide', {
+            type:'loop',
+             // Set the slider type to carousel
+        autoplay: true,   // Enable auto-sliding
+        interval: 3000,   // Set the interval between each slide transition 
+        } ).mount();    
+
